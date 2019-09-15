6d40d85fdc177d18fc31a1bb282046acb96ac51b
Jake <jakepifer3@gmail.com>
2019-09-15 14:00:12 -0400

    pandas working properly

--- app/functions/read_only.py
+++ app/functions/read_only.py
@@ -2,5 +2,6 @@
 import pandas as pd
 
 def get_data():
+        # data = pd.read_csv("movie_metadata.csv", usecols=[9]) 
         data = pd.read_csv("movie_metadata.csv") 
         return data
--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,8 @@ from app.functions.read_only import *
 @app.route('/', methods=['GET'])
 def index():
     data = get_data()
-    print(data.head())
+    for key in data.iloc[2].items():
+        print(key[0])
+    print(data.iloc[2])
     # print(data.text)
     return render_template('mainpage.html', title='Main')
