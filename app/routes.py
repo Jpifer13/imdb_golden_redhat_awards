cbd988c0ca5142964abc8dcefab98ec23ef68800
Jake <jakepifer3@gmail.com>
2019-09-15 14:00:12 -0400

    created database and connected

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,12 @@ import requests
 
 from flask import render_template
 from app import app
-from app.functions.read_only import *
+from app.functions.r import *
+from app.functions.rw import *
 
 @app.route('/', methods=['GET'])
 def index():
-    data = get_data()
-    print(data.head())
+    # data = import_database()
+    
     # print(data.text)
     return render_template('mainpage.html', title='Main')
