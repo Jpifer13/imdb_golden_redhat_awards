63555c6b004fd220913ef68076bb727257aa2768
Jake Pifer <jakepifer3@gmail.com>
2019-09-17 18:36:51 -0400

    Test (#3)
    * Initial build
    
    * pandas working properly
    
    * created database framework
    
    * created database and connected
    
    * created database and connected
    
    * created database and connected

--- golden_redhat_awards.py
+++ golden_redhat_awards.py
@@ -1,10 +1,4 @@
-from app import app, db
-from app.models import Director, Actor, Movie
+# from app import app, db
+from app import app
+# from app.models import Director, Actor, Movie
 
-@app.shell_context_processor
-def make_shell_context():
-    """
-    creates a shell context that adds the database instance and models to the shell session
-    CTRL-Z to close shell
-    """
-    return {'db': db, 'Director': Director, 'Actor': Actor, 'Movie': Movie}
